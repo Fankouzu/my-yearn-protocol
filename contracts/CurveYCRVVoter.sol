0eaea3374ff877cf24346d097def4b30fa593002
lucas <skr1258@163.com>
2020-11-25 10:50:10 +0800

    comment for understand lock and proxy

--- contracts/CurveYCRVVoter.sol
+++ contracts/CurveYCRVVoter.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.7.0;
+pragma solidity ^0.5.17;
 
-import "../@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../@openzeppelin/contracts/math/SafeMath.sol";
-import "../@openzeppelin/contracts/utils/Address.sol";
-import "../@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelinV2/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelinV2/contracts/math/SafeMath.sol";
+import "@openzeppelinV2/contracts/utils/Address.sol";
+import "@openzeppelinV2/contracts/token/ERC20/SafeERC20.sol";
 
 import "../../interfaces/curve/Curve.sol";
 import "../../interfaces/curve/Gauge.sol";
@@ -14,34 +14,64 @@ import "../../interfaces/curve/VoteEscrow.sol";
 import "../../interfaces/uniswap/Uni.sol";
 import "../../interfaces/yearn/IToken.sol";
 
+/**
+* Y池投资代理
+* Lucas
+*/
 contract CurveYCRVVoter {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
 
+    //yDAI+yUSDC+yUSDT+yTUSD 
     address public constant want = address(0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8);
+    //Curve的pool yDAI/yUSDC/yUSDT/yTUSD 
     address public constant pool = address(0xFA712EE4788C042e2B7BB55E6cb8ec569C4530c1);
+    //Curve Token Minter 用于Crv的分发挖矿
     address public constant mintr = address(0xd061D61a4d941c39E5453435B6345Dc261C2fcE0);
+    //crv token
     address public constant crv = address(0xD533a949740bb3306d119CC777fa900bA034cd52);
+    /*
+    @notice Votes have a weight depending on time, so that users are
+            committed to the future of (whatever they are voting for)
+    @dev Vote weight decays linearly over time. Lock time cannot be
+        more than `MAXTIME` (4 years).
+    """
 
+    # Voting escrow to have time-weighted votes
+    # Votes have a weight depending on time, so that users are committed
+    # to the future of (whatever they are voting for).
+    # The weight in this implementation is linear, and lock cannot be more than maxtime:
+    # w ^
+    # 1 +        /
+    #   |      /
+    #   |    /
+    #   |  /
+    #   |/
+    # 0 +--------+------> time
+    #       maxtime (4 years?)
+    */
     address public constant escrow = address(0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2);
 
     address public governance;
     address public strategy;
 
-    constructor() {
+    constructor() public {
         governance = msg.sender;
     }
 
+    //获取名字
     function getName() external pure returns (string memory) {
         return "CurveYCRVVoter";
     }
 
+    //设置策略
     function setStrategy(address _strategy) external {
         require(msg.sender == governance, "!governance");
         strategy = _strategy;
     }
 
+    //y池的投资方法
     function deposit() public {
         uint256 _want = IERC20(want).balanceOf(address(this));
         if (_want > 0) {
@@ -51,6 +81,7 @@ contract CurveYCRVVoter {
         }
     }
 
+    //额外币种取回控制器
     // Controller only function for creating additional rewards from dust
     function withdraw(IERC20 _asset) external returns (uint256 balance) {
         require(msg.sender == strategy, "!controller");
@@ -58,6 +89,7 @@ contract CurveYCRVVoter {
         _asset.safeTransfer(strategy, balance);
     }
 
+    //yDAI+yUSDC+yUSDT+yTUSD 取回到策略
     // Withdraw partial funds, normally used with a vault withdrawal
     function withdraw(uint256 _amount) external {
         require(msg.sender == strategy, "!controller");
@@ -69,6 +101,7 @@ contract CurveYCRVVoter {
         IERC20(want).safeTransfer(strategy, _amount);
     }
 
+    //yDAI+yUSDC+yUSDT+yTUSD  全部取回到策略
     // Withdraw all funds, normally used when migrating strategies
     function withdrawAll() external returns (uint256 balance) {
         require(msg.sender == strategy, "!controller");
@@ -78,10 +111,12 @@ contract CurveYCRVVoter {
         IERC20(want).safeTransfer(strategy, balance);
     }
 
+    //从Curve yDAI+yUSDC+yUSDT+yTUSD  Pool中全部撤回到本合约
     function _withdrawAll() internal {
         Gauge(pool).withdraw(Gauge(pool).balanceOf(address(this)));
     }
 
+    //托管合约，锁定crv
     function createLock(uint256 _value, uint256 _unlockTime) external {
         require(msg.sender == strategy || msg.sender == governance, "!authorized");
         IERC20(crv).safeApprove(escrow, 0);
@@ -89,6 +124,7 @@ contract CurveYCRVVoter {
         VoteEscrow(escrow).create_lock(_value, _unlockTime);
     }
 
+    //托管合约，增加crv
     function increaseAmount(uint256 _value) external {
         require(msg.sender == strategy || msg.sender == governance, "!authorized");
         IERC20(crv).safeApprove(escrow, 0);
@@ -96,33 +132,40 @@ contract CurveYCRVVoter {
         VoteEscrow(escrow).increase_amount(_value);
     }
 
+    //从托管合约取回crv到本合约
     function release() external {
         require(msg.sender == strategy || msg.sender == governance, "!authorized");
         VoteEscrow(escrow).withdraw();
     }
 
+    //从Curve中部分取款
     function _withdrawSome(uint256 _amount) internal returns (uint256) {
         Gauge(pool).withdraw(_amount);
         return _amount;
     }
 
+    //未投资余额
     function balanceOfWant() public view returns (uint256) {
         return IERC20(want).balanceOf(address(this));
     }
 
+    //已投资余额
     function balanceOfPool() public view returns (uint256) {
         return Gauge(pool).balanceOf(address(this));
     }
 
+    //获取总余额
     function balanceOf() public view returns (uint256) {
         return balanceOfWant().add(balanceOfPool());
     }
 
+    //设置治理地址
     function setGovernance(address _governance) external {
         require(msg.sender == governance, "!governance");
         governance = _governance;
     }
 
+    //操作代理，实现了：允许StrategyProxy和治理地址操作本合约的金额
     function execute(
         address to,
         uint256 value,
--- contracts/StrategyProxy.sol
+++ contracts/StrategyProxy.sol
@@ -1,55 +1,74 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.7.0;
+pragma solidity ^0.5.17;
 
-import "../@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../@openzeppelin/contracts/math/SafeMath.sol";
-import "../@openzeppelin/contracts/utils/Address.sol";
-import "../@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelinV2/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelinV2/contracts/math/SafeMath.sol";
+import "@openzeppelinV2/contracts/utils/Address.sol";
+import "@openzeppelinV2/contracts/token/ERC20/SafeERC20.sol";
 
 import "../../interfaces/yearn/IProxy.sol";
 import "../../interfaces/curve/Mintr.sol";
 
+/**
+*本合约功能：
+* 1、Curve 调用接口封装deposit，withdraw，harvest三个方法
+* 2、策略的审核approveStrategy，revokeStrategy，要求功能1调用的策略必须是经过审核的
+* Lucas
+*/
 contract StrategyProxy {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
 
+    //CurveYCRVVoter 合约
     IProxy public constant proxy = IProxy(0xF147b8125d2ef93FB6965Db97D6746952a133934);
+    //Curve Token Minter 用于Crv的分发挖矿
     address public constant mintr = address(0xd061D61a4d941c39E5453435B6345Dc261C2fcE0);
+    //crv token
     address public constant crv = address(0xD533a949740bb3306d119CC777fa900bA034cd52);
+    //Curve的gauge facotry，所有gauge都汇总在此
     address public constant gauge = address(0x2F50D538606Fa9EDD2B11E2446BEb18C9D5846bB);
+    //Curve（本合约未使用）
     address public constant y = address(0xFA712EE4788C042e2B7BB55E6cb8ec569C4530c1);
-
+    //策略的审核结果
     mapping(address => bool) public strategies;
+    //治理地址
     address public governance;
 
     constructor() public {
         governance = msg.sender;
     }
 
+    //设置治理地址
     function setGovernance(address _governance) external {
         require(msg.sender == governance, "!governance");
         governance = _governance;
     }
 
+    //允许策略
     function approveStrategy(address _strategy) external {
         require(msg.sender == governance, "!governance");
         strategies[_strategy] = true;
     }
 
+    //禁止策略
     function revokeStrategy(address _strategy) external {
         require(msg.sender == governance, "!governance");
         strategies[_strategy] = false;
     }
 
     function lock() external {
+        //CurveYCRVVoter合约的Crv
         uint256 amount = IERC20(crv).balanceOf(address(proxy));
+        //增加托管合约中crv的托管金额
         if (amount > 0) proxy.increaseAmount(amount);
     }
 
+    //参与Curve的治理（调整池子各个币种的比例？）
     function vote(address _gauge, uint256 _amount) public {
         require(strategies[msg.sender], "!strategy");
+        //所有资金都在proxy，因此还得通过代理去调用
         proxy.execute(gauge, 0, abi.encodeWithSignature("vote_for_gauge_weights(address,uint256)", _gauge, _amount));
     }
 
@@ -59,41 +78,63 @@ contract StrategyProxy {
         uint256 _amount
     ) public returns (uint256) {
         require(strategies[msg.sender], "!strategy");
+        //CurveYCRVVoter合约上token的金额
         uint256 _before = IERC20(_token).balanceOf(address(proxy));
+        //执行Curve的的取款操作
         proxy.execute(_gauge, 0, abi.encodeWithSignature("withdraw(uint256)", _amount));
+        //取款后token的余额
         uint256 _after = IERC20(_token).balanceOf(address(proxy));
+        //差值
         uint256 _net = _after.sub(_before);
+        //将token的差值转回策略合约，完成撤回投资操作
         proxy.execute(_token, 0, abi.encodeWithSignature("transfer(address,uint256)", msg.sender, _net));
         return _net;
     }
 
+    //获取CurveYCRVVoter投资到Curve某个池子的总金额
     function balanceOf(address _gauge) public view returns (uint256) {
         return IERC20(_gauge).balanceOf(address(proxy));
     }
 
+    //撤回某个token的全部投资
     function withdrawAll(address _gauge, address _token) external returns (uint256) {
+        //限定：审核过的策略
         require(strategies[msg.sender], "!strategy");
+        //调用withdraw
         return withdraw(_gauge, _token, balanceOf(_gauge));
     }
 
+    //投资
     function deposit(address _gauge, address _token) external {
+        //限定：审核过的策略
         require(strategies[msg.sender], "!strategy");
+        //策略发送过来的投资额度
         uint256 _balance = IERC20(_token).balanceOf(address(this));
+        //转发给CurveYCRVVoter
         IERC20(_token).safeTransfer(address(proxy), _balance);
+        //获取CurveYCRVVoter最后的额度
         _balance = IERC20(_token).balanceOf(address(proxy));
-
+        //批准Curve可操作的token
         proxy.execute(_token, 0, abi.encodeWithSignature("approve(address,uint256)", _gauge, 0));
         proxy.execute(_token, 0, abi.encodeWithSignature("approve(address,uint256)", _gauge, _balance));
+        //调用Curve的存款方法，完成投资
         (bool success, ) = proxy.execute(_gauge, 0, abi.encodeWithSignature("deposit(uint256)", _balance));
         if (!success) assert(false);
     }
 
+    //收获Crv
     function harvest(address _gauge) external {
+        //限定：审核过的策略
         require(strategies[msg.sender], "!strategy");
+        //CurveYCRVVoter合约的crv
         uint256 _before = IERC20(crv).balanceOf(address(proxy));
+        //挖crv，得到的crv仍然在CurveYCRVVoter合约中
         proxy.execute(mintr, 0, abi.encodeWithSignature("mint(address)", _gauge));
+        //挖了后的crv
         uint256 _after = IERC20(crv).balanceOf(address(proxy));
+        //本次挖得的crv
         uint256 _balance = _after.sub(_before);
+        //crv发送回策略---策略继续crv的转换，进行复利投资
         proxy.execute(crv, 0, abi.encodeWithSignature("transfer(address,uint256)", msg.sender, _balance));
     }
 }
